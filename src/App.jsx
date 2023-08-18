709abada07a5c237eeeff138b44c00005adb62fb
Victor Montilla <victor5516@gmail.com>
2023-08-18 09:02:20 -0400

    agregar la tareas

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,31 @@ import Button from './components/Button'
 
 
 function App() {
-  const [value, setValue] = useState('')
-  const [todos, setTodos] = useState([])
+  //estados de la aplicacion
+  const [value, setValue] = useState('') // valor del input (la tarea a agregar)
+  const [todos, setTodos] = useState([])// la lista de tareas ( es un array de strings)
 
+  //funcion que agrega las tareas a la lista y elimina el valor del input
   const handleClickAdd = () => {
     setTodos([...todos, value])
     setValue('')
-    console.log(todos)
-  }
 
+  }
+//funcion donde se guarda el valor del input
   const handleChange = (e) => {
     e.preventDefault()
     setValue(e.target.value)
 
   }
-
+//funcion que elimina las tareas de la lista
   const handleDelete = (index) => {
 
     return () => {
+      // obtenemos una copia del array de tareas
       const newTodos = [...todos]
+      // eliminamos la tarea que se encuentra en el indice
       newTodos.splice(index, 1)
+      // actualizamos el estado de la lista de tareas
       setTodos(newTodos)
     }
     //indices = [0,1,2,3,4]
